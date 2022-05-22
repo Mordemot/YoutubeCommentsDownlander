8de6c7bf3f54b67f7b2ebffccb4d71be1ced5852
mordemot <mordemot68@gmail.com>
2022-05-22 12:58:27 +0200

    v1.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='YoutubeCommentsDownlander',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='mordemot',
+    author_email='',
+    description=''
+)
